7748538c1f2fe67553eae2aeea1a16b6ccda3070
matic <msc0101312@gmail.com>
2025-03-19 15:50:07 +0900

    임시2

--- test4/src/ex/ex.java
+++ test4/src/ex/ex.java
@@ -39,6 +39,7 @@ public class ex {
 				System.out.print(rs.getString("bookName") + " ");
 				System.out.print(rs.getString("publisher") + "\n");
 				System.out.print(rs.getString("isbn") + "\n");
+				
 			}
 		}
 		return null;
