2159b832c564ca55d42c5809b33f5afd22efc368
JiwebD <gkdlfnzo12@naver.com>
2025-03-19 15:28:07 +0900

    1

--- test4_1/src/test1/Add.java
+++ test4_1/src/test1/Add.java
@@ -4,7 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Add implements Calculation{
+	List<String> wordList;
+	Add(List<String> wordList){
+		this.wordList = wordList;
+	}
+	
+	@Override
+	public void sort(boolean isAsend) {
+		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void add(String word) {
+		wordList.add(word);
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 
 }
