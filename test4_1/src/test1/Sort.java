f6601cd7d4bfb9d27c0e0acbdc13d77bfc410491
KIMJONGHO <ho098701@naver.com>
2025-03-19 15:10:21 +0900

    add

--- test4_1/src/test1/Sort.java
+++ test4_1/src/test1/Sort.java
@@ -1,9 +1,38 @@
 package test1;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Sort implements Calculation {
+	List <String> li = new ArrayList<>();
+	
+	Sort(List li){
+		this.li = li;
+	}
+	
+	@Override
+	public void sort(boolean isAsend) {
+		// TODO Auto-generated method stub
+		if (isAsend == true) {
+			Collections.sort(li);
+		}else {
+			li.sort(Collections.reverseOrder());
+		}
+		
+	}
 
+	@Override
+	public void add(String word) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
